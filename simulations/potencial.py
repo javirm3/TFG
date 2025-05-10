6580bee3dcda4cf2c8269447e0dc72e7ceae9b45
Javi <jrm2003@icloud.com>
2025-05-10 19:40:19 +0200

    Updated bifurcation analysis and organized code

--- simulations/potencial.py
+++ simulations/potencial.py
@@ -3,14 +3,14 @@ from scipy.optimize import fsolve
 import pickle
 
 
-def get_numeric_functions(values):  
+def get_expressions(values, type='numeric', substituted_I=False):  
     X1, X2 = sp.symbols('X1 X2')
     tau, phi_p, phi_pp, phi_ppp, s0, c, g, phi_Ip = sp.symbols('tau phi_p phi_pp phi_ppp s0 c g phi_Ip')
     I_L, I_C, I_R, s_L, s_C, s_R, R = sp.symbols('I_L I_C I_R s_L s_C s_R R')
     x = sp.symbols('x', real=True)
     R_I, I0, I_I = sp.symbols('R_I I0 I_I', real=True)
 
-    with open('U_simpl.pkl', 'rb') as f:
+    with open('U.pkl', 'rb') as f:
         U_simpl = pickle.load(f)
     with open('field_components.pkl', 'rb') as f:
         exprs = pickle.load(f)
@@ -68,7 +68,7 @@ def get_numeric_functions(values):
     phi_ppp_val = sp.N(phi_triple_prime_X0.subs({R: R_val, R_I: R_I_val, I0: I0_val, s0: values['s0'], c: values['c'], g: values['g']}))
     phi_Ip_val = sp.N(phi_I_prime.subs({R: R_val, R_I: R_I_val, I0: I0_val, s0: values['s0'], c: values['c'], g: values['g'], I_I: values['I_I']}))
 
-    subs_dict = {
+    dict_I = {
         phi_p:   phi_p_val,
         phi_pp:  phi_pp_val,
         phi_ppp: phi_ppp_val,
@@ -86,7 +86,7 @@ def get_numeric_functions(values):
         s_R:     values['sR'],
         R_I:     R_I_val,
     }
-    subs_dict2 = {
+    dict_without_I = {
         phi_p:   phi_p_val,
         phi_pp:  phi_pp_val,
         phi_ppp: phi_ppp_val,
@@ -102,19 +102,30 @@ def get_numeric_functions(values):
         R_I:     R_I_val,
     }
 
-    H = sp.hessian(U_simpl.subs(subs_dict2), (X1, X2))
-    H_num  = sp.lambdify((X1, X2, I_L, I_C, I_R), H,  'numpy')
 
+    if substituted_I:
+        subs_dict =  dict_I
+        lamdifyed = (X1, X2)
+    else:
+        subs_dict = dict_without_I
+        lamdifyed = (X1, X2, I_L, I_C, I_R)
+
+   
+
+    H_sim = sp.hessian(U_simpl.subs(subs_dict), (X1, X2))
     U_sim = sp.simplify(U_simpl.subs(subs_dict))
     F1_sim = sp.simplify(F1.subs(subs_dict))
     F2_sim = sp.simplify(F2.subs(subs_dict))
 
-    U_num_expr  = sp.simplify(U_simpl.subs(subs_dict2))
-    F1_num_expr = sp.simplify(F1.subs(subs_dict2))
-    F2_num_expr = sp.simplify(F2.subs(subs_dict2))
+    H_num  = sp.lambdify(lamdifyed, H_sim,  'numpy')
+    U_num  = sp.lambdify(lamdifyed, U_sim,  'numpy')
+    F1_num = sp.lambdify(lamdifyed, F1_sim, 'numpy')
+    F2_num = sp.lambdify(lamdifyed, F2_sim, 'numpy')
 
-    U_num  = sp.lambdify((X1, X2, I_L, I_C, I_R), U_num_expr,  'numpy')
-    F1_num = sp.lambdify((X1, X2, I_L, I_C, I_R), F1_num_expr, 'numpy')
-    F2_num = sp.lambdify((X1, X2, I_L, I_C, I_R), F2_num_expr, 'numpy')
-   
-    return U_sim,F1_sim,F2_sim, U_num, F1_num, F2_num, H
+    if(type == 'numeric'):
+        return U_num, F1_num, F2_num, H_num
+    elif(type == 'symbolic'):
+        return U_sim, F1_sim, F2_sim, H_sim
+    else: 
+        raise ValueError("Invalid type. Use 'numeric' or 'symbolic'.")
+    
